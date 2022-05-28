cc00551953e8baeca25835991bd487b554af3617
sourabhkumhar <mr.sourabhkumhar@gmail.com>
2022-05-28 14:02:54 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
 
-$servername = 'localhost';
-$username = 'root';
-$password = '';
-$database = 'yottol';
+$servername = 'remotemysql.com';
+$username = '4lkcOMDRFj';
+$password = 'PcLAMlQhz4';
+$database = '4lkcOMDRFj';
 
 $conn = mysqli_connect($servername, $username, $password, $database);
 
