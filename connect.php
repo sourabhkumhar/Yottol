c20b0cb0cbe78d4fff69645473d9661f0575991d
sourabhkumhar <mr.sourabhkumhar@gmail.com>
2022-05-28 14:18:47 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,11 +1,13 @@
 <?php
 
-$servername = 'remotemysql.com';
+$servername = 'remotemysql.com:3306';
 $username = '4lkcOMDRFj';
 $password = 'PcLAMlQhz4';
 $database = '4lkcOMDRFj';
 
 $conn = mysqli_connect($servername, $username, $password, $database);
+
+mysqli_report(MYSQLI_REPORT_ALL ^ MYSQLI_REPORT_INDEX);
 
 if(!$conn) {
     die('Connection Error: '.mysqli_connect_error());
